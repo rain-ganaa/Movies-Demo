5b5d21b38bf30cea407b1d16f6dff3e62abbf0fa
Gan-Erdene <rain.lp99@gmail.com>
2024-02-24 19:37:32 +1000

    Initial Commit

---
+++ Movies Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies Demo
+//
+//  Created by Rain on 2024.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
